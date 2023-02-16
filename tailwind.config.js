aaa3289a4b7d54b56c092c8c507b3e9805e81c38
Konstantinos Karadamoglou <kkarad@gmail.com>
2023-02-16 00:48:21 +0000

    implemented fonts, spacing and responsive design; added README.md

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,19 @@ module.exports = {
     "./index.html"
   ],
   theme: {
-    colors: {
-        transparent: "transparent",
-        current: "currentColor",
-        white: "hsl(0, 0%, 100%)",
-        "light-gray": "hsl(212, 45%, 89%)",
-        "grayish-blue": "hsl(220, 15%, 55%)",
-        "dark-blue": "hsl(218, 44%, 22%)"
+    extend: {
+        colors: {
+            transparent: "transparent",
+            current: "currentColor",
+            white: "hsl(0, 0%, 100%)",
+            "light-gray": "hsl(212, 45%, 89%)",
+            "grayish-blue": "hsl(220, 15%, 55%)",
+            "dark-blue": "hsl(218, 44%, 22%)"
+        },
+        fontFamily: {
+            'display': ['"Outfit"', 'sans-serif']
+        }
     },
-    extend: {},
   },
   plugins: [],
 }
